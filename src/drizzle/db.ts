14cb36ad0e873a5ce67db54fd0d6e9e735d1f4ec
Lua Viduedo <luaviduedo@gmail.com>
2026-02-11 16:40:04 -0300

    fix: import `schema` to `db.ts`

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,10 +1,13 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 
+import * as schema from "@/src/drizzle/schema";
+
 const db = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === "production" ? true : false,
   },
+  schema: schema,
 });
 
 export default db;
