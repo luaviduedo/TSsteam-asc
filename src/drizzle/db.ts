97dee85f99af447570529f9b011b70e85d114624
Lua Viduedo <luaviduedo@gmail.com>
2026-02-13 17:13:55 -0300

    refactor `db.ts`

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,26 @@
+import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
-
+import { Pool } from "pg";
 import * as schema from "@/src/drizzle/schema";
 
-const db = drizzle({
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
-  },
-  schema: schema,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: process.env.NODE_ENV === "production" ? true : false,
 });
+const db = drizzle({ client: pool, schema: schema });
 
 export default db;
+
+// import { drizzle } from "drizzle-orm/node-postgres";
+
+// import * as schema from "@/src/drizzle/schema";
+
+// const db = drizzle({
+//   connection: {
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: process.env.NODE_ENV === "production" ? true : false,
+//   },
+//   schema: schema,
+// });
+
+// export default db;
