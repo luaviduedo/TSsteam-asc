2835e9b99b715139f1180954adc684a339c5fea1
Lua Viduedo <luaviduedo@gmail.com>
2026-02-25 14:45:32 -0300

    create new migration and config database

--- jest.config.ts
+++
@@ -1,7 +0,0 @@
-import type { Config } from "jest";
-
-const config: Config = {
-  moduleDirectories: ["node_modules", "<rootDir>"],
-};
-
-export default config;
--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "@/src/drizzle/schema";
+import * as schema from "../drizzle/schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL!,
