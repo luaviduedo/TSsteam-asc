9d3fc052cc4fef6c6dfb38b44ef5b8dbd860699b
Lua Viduedo <luaviduedo@gmail.com>
2026-03-03 20:37:23 -0300

    remove users query from endpoint

--- src/app/api/v1/status/route.tsx
+++ src/app/api/v1/status/route.tsx
@@ -4,8 +4,6 @@ import { QueryResultRow } from "@neondatabase/serverless";
 export async function GET() {
   const updatedAt = new Date().toISOString();
 
-  const usersResult = await db.query.usersTable.findMany();
-
   const databaseVersionResult = await db.execute("SHOW server_version");
   const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
@@ -27,7 +25,6 @@ export async function GET() {
   return Response.json(
     {
       updated_at: updatedAt,
-      users: usersResult,
       dependencies: {
         database: {
           version: databaseVersionValue,
