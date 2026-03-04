dd322f16bf18b9083f859a6b930de6a998e19776
Lua Viduedo <luaviduedo@gmail.com>
2026-03-04 12:13:02 -0300

    edit html link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,9 +28,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <header className="siteHeader">
           <div className="siteHeaderInner">
-            <a href="/" className="siteTitle">
+            <Link href="/" className="siteTitle">
               Steam ASC
-            </a>
+            </Link>
           </div>
         </header>
 
