429072861e19263273342bf4d7b182ea85523175
Lua Viduedo <luaviduedo@gmail.com>
2026-03-09 12:10:43 -0300

    make `route.ts` more robust to errors

--- src/app/api/v1/steam_user/route.ts
+++ src/app/api/v1/steam_user/route.ts
@@ -3,32 +3,22 @@ export async function POST(request: Request) {
   const requestBody = await request.json();
   const steamId64Search = requestBody.req_steam_id_64;
 
-  try {
-    const steamIdRegex = /^\d{17}$/;
+  const steamIdRegex = /^\d{17}$/;
 
-    if (!steamIdRegex.test(steamId64Search)) {
-      return Response.json({ error: "SteamID64 inválido." }, { status: 400 });
-    }
-
-    async function fetchSteamUser(steamKey: string, steamId64: string) {
-      const response = await fetch(
-        `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId64}`,
-      );
-      const responseBody = await response.json();
-
-      return responseBody;
-    }
-
-    const { response } = await fetchSteamUser(steamKeyAPI, steamId64Search);
-    const steamNickName = response.players[0].personaname;
-
-    return Response.json({ STEAM: steamNickName }, { status: 200 });
-  } catch (error) {
-    console.error("Erro interno:", error);
-
-    return Response.json(
-      { error: "Erro interno do servidor." },
-      { status: 504 },
-    );
+  if (!steamIdRegex.test(steamId64Search)) {
+    return Response.json({ error: "SteamID64 inválido." }, { status: 400 });
   }
+  async function fetchSteamUser(steamKey: string, steamId64: string) {
+    const response = await fetch(
+      `https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamId64}`,
+    );
+    const responseBody = await response.json();
+
+    return responseBody;
+  }
+
+  const { response } = await fetchSteamUser(steamKeyAPI, steamId64Search);
+  const steamNickName = response.players[0].personaname;
+
+  return Response.json({ STEAM: steamNickName }, { status: 200 });
 }
