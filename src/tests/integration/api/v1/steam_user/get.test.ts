12eb49c8abc6a2fc04d79f8f5a48921bc49d5ef0
Lua Viduedo <luaviduedo@gmail.com>
2026-03-09 11:36:20 -0300

    create tests to `/api/v1/steam_user` endpoint

---
+++ src/tests/integration/api/v1/steam_user/get.test.ts
@@ -0,0 +1,44 @@
+import orchestrator from "tests/orchestrator";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+});
+
+describe("POST /api/v1/steam_user", () => {
+  describe("Anonymous user", () => {
+    test("With correct `steamId64`", async () => {
+      const correctSteamId64 = "76561198145040749";
+      const response = await fetch("http://localhost:3000/api/v1/steam_user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          req_steam_id_64: correctSteamId64,
+        }),
+      });
+      expect(response.status).toBe(200);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({ STEAM: "rodolfo old #fimdoracismo" });
+    });
+    test("With incorrect `steamId64`", async () => {
+      const correctSteamId64 = "7656119814504074";
+      const response = await fetch("http://localhost:3000/api/v1/steam_user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          req_steam_id_64: correctSteamId64,
+        }),
+      });
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({ error: "SteamID64 inválido." });
+    });
+  });
+});
