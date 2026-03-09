4442ebe7976189604c7e865ba78f12417053152f
Lua Viduedo <luaviduedo@gmail.com>
2026-03-09 12:37:39 -0300

    commit

--- src/tests/integration/api/v1/steam_user/get.test.ts
+++ src/tests/integration/api/v1/steam_user/get.test.ts
@@ -6,23 +6,6 @@ beforeAll(async () => {
 
 describe("POST /api/v1/steam_user", () => {
   describe("Anonymous user", () => {
-    test("With incorrect `steamId64`", async () => {
-      const inCorrectSteamId64 = "7656119814504074";
-      const response = await fetch("http://localhost:3000/api/v1/steam_user", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          req_steam_id_64: inCorrectSteamId64,
-        }),
-      });
-      expect(response.status).toBe(400);
-
-      const responseBody = await response.json();
-
-      expect(responseBody).toEqual({ error: "SteamID64 inválido." });
-    });
     test("With correct `steamId64`", async () => {
       const correctSteamId64 = "76561198145040749";
       const response = await fetch("http://localhost:3000/api/v1/steam_user", {
@@ -41,6 +24,23 @@ describe("POST /api/v1/steam_user", () => {
       const responseBody = await response.json();
 
       expect(responseBody).toEqual({ STEAM: "rodolfo old #fimdoracismo" });
+    });
+    test("With incorrect `steamId64`", async () => {
+      const inCorrectSteamId64 = "7656119814504074";
+      const response = await fetch("http://localhost:3000/api/v1/steam_user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          req_steam_id_64: inCorrectSteamId64,
+        }),
+      });
+      expect(response.status).toBe(400);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({ error: "SteamID64 inválido." });
     });
   });
 });
