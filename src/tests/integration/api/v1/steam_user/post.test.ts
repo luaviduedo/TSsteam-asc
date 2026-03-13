be2eb7b75dcfaaa0803c77b9e55b647687b99e62
Lua Viduedo <luaviduedo@gmail.com>
2026-03-13 17:12:52 -0300

    edit test

--- src/tests/integration/api/v1/steam_user/post.test.ts
+++ src/tests/integration/api/v1/steam_user/post.test.ts
@@ -26,23 +26,6 @@ describe("POST /api/v1/steam_user", () => {
 
       expect(results.personaname).toEqual("rodolfo old #fimdoracismo");
     });
-    test("With incorrect `steamId64`", async () => {
-      const inCorrectSteamId64 = "7656119814504074";
-      const response = await fetch("http://localhost:3000/api/v1/steam_user", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          req_steam_id_64: inCorrectSteamId64,
-        }),
-      });
-      expect(response.status).toBe(400);
-
-      const responseBody = await response.json();
-
-      expect(responseBody).toEqual({ error: "SteamID64 inválido." });
-    });
     test("With no data", async () => {
       const response = await fetch("http://localhost:3000/api/v1/steam_user", {
         method: "POST",
@@ -52,7 +35,7 @@ describe("POST /api/v1/steam_user", () => {
       const responseBody = await response.json();
 
       expect(responseBody).toEqual({
-        error: "Body da requisição precisa ser JSON válido.",
+        error: "Body inválido. Envie um JSON válido.",
       });
     });
   });
